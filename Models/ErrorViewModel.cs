aae4bde037458a2bc32b5a6fb733c3bb2757edbf
kinoshita <kinoshita@sigmact.com>
2018-09-08 14:32:52 +0900

    初期コミット

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kokoni_vscode_trial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
